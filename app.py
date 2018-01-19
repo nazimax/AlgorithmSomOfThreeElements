a1f17ce5afb82243152ee9c3eb15a55f4a59aefa
nazimax <kissi.salim.yahia@outlook.fr>
2018-01-19 22:00:54 +0100

    the algorithm is working without heapsort which means heapsort is in TODO list

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
+# TODO finish heapsort
 def left(i):
-    return 2 * i
+    if i == 0:
+        return 1
+    else:
+        return 2 * i
 
 
 def right(i):
-    return 2 * i + 1
+    if i == 0:
+        return 2
+    else:
+        return 2 * i + 1
 
 
 def heap(T, i):
@@ -21,5 +28,56 @@ def heap(T, i):
 
 
 def buildHeap(T):
-    i=len(T)/2
+    i = (len(T) / 2)
+    while i >= 0:
+        heap(T, i)
+        i = i - 1
     return 0
+
+
+# we suppose that heapsort is done now start the alorithm
+# we test it with sorted array
+
+
+# these two functions return the indexes of numbers
+
+def sumTwoElements(T, toFind):
+    fromRight = len(T) - 1
+    fromLeft = 0
+    i = 0
+    som = T[fromLeft] + T[fromRight]
+    while som != toFind and fromLeft < fromRight:
+        if som > toFind:
+            fromLeft = fromLeft + 1
+            som = T[fromLeft] + T[fromRight]
+        else:
+            fromRight = fromRight - 1
+            som = T[fromLeft] + T[fromRight]
+
+    if fromLeft < fromRight:
+        return fromLeft, fromRight
+    else:
+        return -1, -1
+
+
+def sumOfThreeElements(T, toFind):
+    k = 0
+    i, j = -1, -1
+    while k < len(T):
+        diff = toFind - T[k]
+        temp = T
+        temp.remove(T[k])
+        i, j = sumTwoElements(temp, diff)
+        if i != j:
+            return k, i, j
+        else:
+            k = k + 1
+
+    return -1, -1, -1
+
+
+a = [20, 18, 17, 15, 12, 8, 4, 1]
+
+print sumOfThreeElements(a, 32)
+
+# finished
